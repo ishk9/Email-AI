c6214f7a8a0fd713bff776b5364d073829c9e27f
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-01-20 16:09:37 +0530

    Changed the theme from white to black!

--- components/Navbar/PlayStoreButton.js
+++ components/Navbar/PlayStoreButton.js
@@ -4,11 +4,15 @@ import { IoLogoGooglePlaystore } from "react-icons/io5";
 export function PlayStoreButton(){
     return (
         <>
-            <button className="flex flex-row items-center justify-center">
-                <IoLogoGooglePlaystore />
+            <button 
+                className="h-10 w-30 p-2 flex flex-row 
+                items-center justify-center border-2 rounded-lg
+                bg-black"
+            >
+                <IoLogoGooglePlaystore color="white"/>
                 <div className="flex flex-col ml-1">
-                    <h1 className="text-xs">Download on the</h1>
-                    <h1 className="text-base">Play Store</h1>
+                    <h1 className="text-xs text-white">Download on</h1>
+                    <h1 className="text-xs text-white font-semibold">Play Store</h1>
                 </div>
             </button>
         </>
