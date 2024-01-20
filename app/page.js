b02ed432fd964e3777a46f02828c76439a2533cc
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-01-20 20:43:53 +0530

    Replaced use state with state management features!

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
+'use client';
+import React from "react";
 import { ContentInputField } from "@/components/ContentInputField/ContentInputField";
 import { Navbar } from "@/components/Navbar/Navbar";
+import { SignIn } from "@/components/SignIn/SignIn";
+
+import useStore from "./store";
 
 export default function Home() {
+  const show = useStore((state) => state.show)
+
   return (
     <main className="flex flex-col w-screen h-screen bg-white">
         <Navbar />
@@ -10,16 +17,19 @@ export default function Home() {
           <h1 className="text-black font-semibold text-4xl">
             AI Mails
           </h1>
-          <p className="text-gray-500 mt-1">
+          <p className="text-black mt-1">
             emails generated and counting!
           </p>
 
           <div className="flex justify-center items-center w-full mt-8">
             <ContentInputField />
           </div>
-
           
-    
+          {show && (
+            <div className="fixed bottom-0 h-1/2 w-full bg-white border-t border-gray-200">
+              <SignIn />
+            </div>
+          )}
 
         </div>
     </main>
