f977f0fd4e77c1cadbed90d3f8999ed96bedaea3
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-01-20 16:09:59 +0530

    Changed the bg and fonts!

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { HiOutlineMail } from "react-icons/hi";
 import { PlayStoreButton } from "./PlayStoreButton";
+import { UserButton } from "./UserButton";
 
 
 export function Navbar(){
     return(
         <>
-            <navbar className="flex flex-row justify-around items-center h-16 p-4 w-full bg-black">
+            <navbar className="flex flex-row justify-around items-center h-16 p-4 w-full bg-white">
                 <div className="flex justify-center items-center">
-                    <HiOutlineMail />
-                    <h1 className="ml-1">Email.Ai</h1>
+                    <HiOutlineMail color="black" size={20}/>
+                    <h1 className="ml-1 text-black text-lg font-semibold">Email.Ai</h1>
                 </div>
 
-                <div>
+                <div className="flex justify-center items-center">
                     <PlayStoreButton />
+                    <UserButton />
                 </div>
             </navbar>
         </>
