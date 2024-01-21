c5bfb86922142ca6fdaab3386377b0e6f72668e8
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-01-21 11:36:44 +0530

    Added icons for ops on generated texts

--- app/page.js
+++ app/page.js
@@ -3,6 +3,10 @@ import React from "react";
 import { ContentInputField } from "@/components/ContentInputField/ContentInputField";
 import { Navbar } from "@/components/Navbar/Navbar";
 import { SignIn } from "@/components/SignIn/SignIn";
+import { FiClipboard } from "react-icons/fi";
+import { MdEdit } from "react-icons/md";
+import { AiOutlineRedo } from "react-icons/ai";
+
 
 import useStore from "./store";
 
@@ -10,7 +14,7 @@ export default function Home() {
   const show = useStore((state) => state.show)
 
   return (
-    <main className="flex flex-col w-screen h-screen bg-white">
+    <main className="flex flex-col w-screen h-screen bg-white overflow-hidden">
         <Navbar />
 
         <div className="h-screen w-screen flex flex-col items-center mt-20">
@@ -30,6 +34,71 @@ export default function Home() {
               <SignIn />
             </div>
           )}
+
+          <div className="flex  flex-col justify-top items-center mt-10">
+            <div className="flex flex-row w-3/4 mb-2 items-end justify-end">
+              <button className="transition-transform transform hover:scale-125">
+                <FiClipboard color="black"/>
+              </button>
+              <button className="transition-transform transform hover:scale-125">
+                <MdEdit color="black"/>
+              </button>
+              <button className="transition-transform transform hover:scale-125">
+                <AiOutlineRedo color="black"/>
+              </button>
+            </div>
+            <div 
+              class="container h-1/2 w-3/4  p-6
+              bg-white border border-gray-300 rounded-md shadow-md
+              flex flex-col items-end"
+              >
+
+              <p class="text-black overflow-y-scroll h-full">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ... (your content)
+              </p>
+            </div>
+          </div>
 
         </div>
     </main>
