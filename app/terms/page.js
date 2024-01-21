387335e1b036eb577ba890d33577902389e0fbfd
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-01-21 14:48:23 +0530

    Added terms page!

---
+++ app/terms/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TermsOfUse() {
+    return (
+        <div className="h-screen w-screen bg-white">
+            <h1 className="text-black">Terms</h1>
+        </div>
+    )
+}
+
+export default TermsOfUse
