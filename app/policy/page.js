ab490f2ac42fa956901e66803bf70ffa1e67ac53
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-01-21 14:48:01 +0530

    Added Policy page!

---
+++ app/policy/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Policy() {
+    return (
+      <div className="h-screen w-screen bg-white">
+        <h1 className="text-black">Policy</h1>
+      </div>
+    )
+}
+
+export default Policy
