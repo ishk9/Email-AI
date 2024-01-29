6fd8ed27467739eb679a1fa484717d7f6c9cdf71
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-01-29 14:30:26 +0530

    Changed bg color to transparent!

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ import UserButton from "./UserButton";
 export default function Navbar(){
     return(
         <>
-            <navbar className="flex flex-row justify-around items-center h-16 p-4 w-full bg-white">
+            <navbar className="flex flex-row justify-around items-center h-16 p-4 w-full bg-transparent">
                 <div className="flex justify-center items-center">
                     <HiOutlineMail color="black" size={20}/>
                     <h1 className="ml-1 text-black text-lg font-semibold">Email.Ai</h1>
